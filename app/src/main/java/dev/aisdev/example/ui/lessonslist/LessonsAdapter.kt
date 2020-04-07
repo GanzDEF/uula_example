f0a7e34b146c9e4ae3bf52c296db2fa71d506834
GanzDEF <efimoffda@gmail.com>
2020-04-08 01:05:52 +0300

    some updates for activity layout

--- app/src/main/java/dev/aisdev/example/ui/lessonslist/LessonsAdapter.kt
+++ app/src/main/java/dev/aisdev/example/ui/lessonslist/LessonsAdapter.kt
@@ -11,34 +11,36 @@ class LessonsAdapter(listener: LessonsListener) : BasePaginationAdapter<Any>(
     providers = mapOf(
         VIDEO_ITEM_LAYOUT to viewHolderProvider { VideoItemViewHolder(it, listener) },
         OFFLINE_MATERIAL_ITEM_LAYOUT to viewHolderProvider { OfflineMaterialItemViewHolder(it, listener) },
-        SURVEY_ITEM_LAYOUT to viewHolderProvider { SurveyItemViewHolder(it, listener) }
-//        HEADER_ITEM_LAYOUT to viewHolderProvider { HeaderItemViewHolder(it, listener) },
-//        FOOTER_ITEM_LAYOUT to viewHolderProvider { FooterItemViewHolder(it, listener) }
-    )
-    , typeSelector = {
+        SURVEY_ITEM_LAYOUT to viewHolderProvider { SurveyItemViewHolder(it, listener) },
+        HEADER_ITEM_LAYOUT to viewHolderProvider { HeaderItemViewHolder(it, listener) },
+        FOOTER_ITEM_LAYOUT to viewHolderProvider { FooterItemViewHolder(it, listener) }
+    ),
+    typeSelector = {
         when (it) {
             is LessonVideo -> VIDEO_ITEM_LAYOUT
-//            is LessonHeader -> HEADER_ITEM_LAYOUT
             is LessonSurvey -> SURVEY_ITEM_LAYOUT
             is LessonOfflineMaterial -> OFFLINE_MATERIAL_ITEM_LAYOUT
-//            is LessonFooter -> FOOTER_ITEM_LAYOUT
+            is LessonHeader -> HEADER_ITEM_LAYOUT
+            is LessonFooter -> FOOTER_ITEM_LAYOUT
             else -> -1
-        }}){
+        }
+    }
+){
 
     companion object {
         const val VIDEO_ITEM_LAYOUT = R.layout.view_lesson_video_detail
         const val OFFLINE_MATERIAL_ITEM_LAYOUT = R.layout.view_lesson_offline_material_detail
         const val SURVEY_ITEM_LAYOUT = R.layout.view_lesson_survey_detail
-//        const val HEADER_ITEM_LAYOUT = R.layout.view_lesson_header_detail
-//        const val FOOTER_ITEM_LAYOUT = R.layout.view_lesson_detail
+        const val HEADER_ITEM_LAYOUT = R.layout.view_lesson_header_detail
+        const val FOOTER_ITEM_LAYOUT = R.layout.view_lesson_navigation
     }
 
     interface LessonsListener {
         fun onVideoClicked(itemId: String)
         fun onOfflineMaterialClicked(itemId: String)
         fun onSurveyClicked(itemId: String)
-//        fun onPrevClicked(itemId: String)
-//        fun onNextClicked(itemId: String)
+        fun onPrevClicked(itemId: String)
+        fun onNextClicked(itemId: String)
     }
 
 }
