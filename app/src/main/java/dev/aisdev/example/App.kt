59ef6293d2f793e8da4e153c02cc5e2adf09bee4
GanzDEF <efimoffda@gmail.com>
2020-04-07 13:54:47 +0300

    wiped out dataBinding for now

--- app/src/main/java/dev/aisdev/example/App.kt
+++ app/src/main/java/dev/aisdev/example/App.kt
@@ -1,8 +1,36 @@
 package dev.aisdev.example
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
+import dev.aisdev.example.di.Scopes
+import dev.aisdev.example.di.modules.AppModule
+import dev.aisdev.example.di.modules.ConvertersModule
+import dev.aisdev.example.di.modules.ServerModule
+import toothpick.Toothpick
+import toothpick.configuration.Configuration
 
-class App : Application() {
+class App : MultiDexApplication() {
 
+    override fun onCreate() {
+        super.onCreate()
 
+        initDi()
+        initAppScope()
+    }
+    private fun initDi() {
+        if (BuildConfig.DEBUG) {
+            Toothpick.setConfiguration(Configuration.forDevelopment().preventMultipleRootScopes())
+        } else {
+            Toothpick.setConfiguration(Configuration.forProduction())
+        }
+    }
+
+    private fun initAppScope() {
+        val appScope = Toothpick.openScope(Scopes.APP_SCOPE)
+        appScope.installModules(AppModule(this))
+
+        val serverScope = Toothpick.openScopes(Scopes.APP_SCOPE, Scopes.SERVER_SCOPE)
+        serverScope.installModules(ServerModule(BuildConfig.API_URL))
+        serverScope.installModules(ServerModule(BuildConfig.API_URL), ConvertersModule())
+    }
 }
