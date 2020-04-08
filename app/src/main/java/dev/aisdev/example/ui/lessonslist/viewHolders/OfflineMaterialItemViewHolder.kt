4a53fe52831dd318a6f28ea4d6a7e973e5707308
GanzDEF <efimoffda@gmail.com>
2020-04-08 22:35:48 +0300

    added viewHolders for rest of types

--- app/src/main/java/dev/aisdev/example/ui/lessonslist/viewHolders/OfflineMaterialItemViewHolder.kt
+++ app/src/main/java/dev/aisdev/example/ui/lessonslist/viewHolders/OfflineMaterialItemViewHolder.kt
@@ -1,16 +1,33 @@
 package dev.aisdev.example.ui.lessonslist.viewHolders
 
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.view.View
+import androidx.core.content.ContextCompat
+import dev.aisdev.example.R
 import dev.aisdev.example.entities.lesson.LessonOfflineMaterial
+import dev.aisdev.example.extentions.visible
 import dev.aisdev.example.ui.base.adapters.BaseMultiTypeAdapter
 import dev.aisdev.example.ui.lessonslist.LessonsAdapter
+import kotlinx.android.synthetic.main.view_lesson_offline_material_detail.view.*
 
 class OfflineMaterialItemViewHolder(
     itemView: View,
-    listener: LessonsAdapter.LessonsListener
+    private val listener: LessonsAdapter.LessonsListener
 ) : BaseMultiTypeAdapter.ViewHolder<Any>(itemView){
-    override fun bindView(item: Any) = (item as LessonOfflineMaterial).let {
 
+    override fun bindView(item: Any) = (item as LessonOfflineMaterial).let {
+        itemView.run {
+            setOnClickListener { listener.onSurveyClicked(it.id.toString()) }
+            tvLessonTitle.text = it.title
+            ivIconContent.setImageDrawable(context.getDrawable(R.drawable.file_icon))
+            ivIconContent.colorFilter = PorterDuffColorFilter(
+                ContextCompat.getColor(context, R.color.colorTextGrey),
+                PorterDuff.Mode.SRC_ATOP
+            )
+            tvCountContentItems.text = String.format("%s", it.format.toString())
+            ivIsVisited.visible(it.visited)
+        }
     }
 
 }
--- app/src/main/java/dev/aisdev/example/ui/lessonslist/viewHolders/SurveyItemViewHolder.kt
+++ app/src/main/java/dev/aisdev/example/ui/lessonslist/viewHolders/SurveyItemViewHolder.kt
@@ -1,17 +1,33 @@
 package dev.aisdev.example.ui.lessonslist.viewHolders
 
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.view.View
+import androidx.core.content.ContextCompat
+import dev.aisdev.example.R
 import dev.aisdev.example.entities.lesson.LessonSurvey
+import dev.aisdev.example.extentions.visible
 import dev.aisdev.example.ui.base.adapters.BaseMultiTypeAdapter
 import dev.aisdev.example.ui.lessonslist.LessonsAdapter
+import kotlinx.android.synthetic.main.view_lesson_survey_detail.view.*
 
 class SurveyItemViewHolder(
     itemView: View,
-    listener: LessonsAdapter.LessonsListener
+    private val listener: LessonsAdapter.LessonsListener
 ) : BaseMultiTypeAdapter.ViewHolder<Any>(itemView){
 
     override fun bindView(item: Any) = (item as LessonSurvey).let {
-
+        itemView.run {
+            setOnClickListener { listener.onSurveyClicked(it.id.toString()) }
+            tvLessonTitle.text = it.title
+            ivIconContent.setImageDrawable(context.getDrawable(R.drawable.file_icon))
+            ivIconContent.colorFilter = PorterDuffColorFilter(
+                ContextCompat.getColor(context, R.color.colorTextGrey),
+                PorterDuff.Mode.SRC_ATOP
+            )
+            tvCountContentItems.text = String.format("%s questions", it.question_count?.toString() ?: "0")
+            ivIsVisited.visible(it.visited)
+        }
     }
 
 }
